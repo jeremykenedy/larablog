6340d36296c9d637ed618e7ae3dc76689d578ed2
jeremykenedy <jeremykenedy@gmail.com>
2018-10-13 00:22:25 -0700

    add phpinfo package

---
+++ config/laravelPhpInfo.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel PHP Info settings
+    |--------------------------------------------------------------------------
+    */
+
+    // The parent blade file
+    'laravelPhpInfoBladeExtended'   => 'laravelusers::layouts.app', // 'blog.layouts.app',
+
+    // Enable `auth` middleware
+    'authEnabled'                   => true,
+
+    // Enable Optional Roles Middleware
+    'rolesEnabled'                  => true,
+
+    // Optional Roles Middleware
+    'rolesMiddlware'                => 'permission:perms.super.admin',
+
+    // Switch Between bootstrap 3 `panel` and bootstrap 4 `card` classes
+    'bootstapVersion'               => '4',
+
+    // Additional Card classes for styling -
+    // See: https://getbootstrap.com/docs/4.0/components/card/#background-and-color
+    // Example classes: 'text-white bg-primary mb-3'
+    'bootstrapCardClasses'          => '',
+
+    // Inline CSS
+    'usePHPinfoCSS'                 => true,
+
+];
